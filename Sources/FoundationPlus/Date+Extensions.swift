db16ad4d8bd99af053ffdc6ad4112b9b50c14849
Tom Hartnett <tomhartnett@gmail.com>
2021-11-26 09:42:53 -0600

    Switched from Calendar.current to .autoupdatingCurrent.

--- Sources/FoundationPlus/Date+Extensions.swift
+++ Sources/FoundationPlus/Date+Extensions.swift
@@ -10,7 +10,7 @@ import Foundation
 public extension Date {
     /// Returns an Int value equal to the hour of the given date.
     var hour: Int {
-        let comps = Calendar.current.dateComponents([.hour], from: self)
+        let comps = Calendar.autoupdatingCurrent.dateComponents([.hour], from: self)
         guard let hour = comps.hour else {
             fatalError("Failed to get hour of \(self)")
         }
@@ -24,13 +24,13 @@ public extension Date {
 
     /// Returns an array of `Date` values for the week of the given date. The week starts on Sunday, and the first value in the array is the Sunday of the week containing the given date.
     var weekDates: [Date] {
-        guard let weekInterval = Calendar.current.dateInterval(of: .weekOfMonth, for: self) else {
+        guard let weekInterval = Calendar.autoupdatingCurrent.dateInterval(of: .weekOfMonth, for: self) else {
             fatalError("Failed to get week interval for \(self)")
         }
 
         var dates = [Date]()
         for index in 0...6 {
-            guard let weekDayDate = Calendar.current.date(byAdding: .day, value: index, to: weekInterval.start) else {
+            guard let weekDayDate = Calendar.autoupdatingCurrent.date(byAdding: .day, value: index, to: weekInterval.start) else {
                 fatalError("Failed to get week day date for \(weekInterval.start) + \(index) days")
             }
             dates.append(weekDayDate)
@@ -41,7 +41,7 @@ public extension Date {
 
     /// Returns an Int value equal to the weekday of the given date. The week starts on Sunday, and the weekday value for Sunday is 1.
     var weekday: Int {
-        let comps = Calendar.current.dateComponents([.weekday], from: self)
+        let comps = Calendar.autoupdatingCurrent.dateComponents([.weekday], from: self)
         guard let weekday = comps.weekday else {
             fatalError("Failed to get weekday of \(self)")
         }
@@ -52,7 +52,7 @@ public extension Date {
     func getTrailingHours(_ number: Int) -> [Int] {
         var hours = [Int]()
         for index in 0..<number {
-            guard let previousDate = Calendar.current.date(byAdding: .hour, value: -index, to: self) else { continue }
+            guard let previousDate = Calendar.autoupdatingCurrent.date(byAdding: .hour, value: -index, to: self) else { continue }
             hours.insert(previousDate.hour, at: 0)
         }
         return hours
